8b05adc5caa0b3072fd8ebbf2d438ac79355b745
yan771012@gmail.com <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>
2023-09-02 10:56:43 +0800

    format code

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,41 +7,41 @@ router.get('/', (req, res) => {
 })
 
 router.get('/productList', async (req, res) => {
-  let products = await Product.findAll({raw: true})
+  const products = await Product.findAll({raw: true})
   res.render('productList', {products})
 })
-//
-// router.get('/customerList', async (req, res) => {
-//   let customers = await Customer.findAll({raw: true})
-//   let products = await Product.findAll({raw: true})
-//
-//   customers = customers.map(customer => {
-//     let favoriteProduct = products.find(product => customer.favoriteId === product.id)
-//     customer.productName = favoriteProduct.name
-//     return customer
-//   })
-//
-//   //模擬其他資料邏輯處理
-//   await saveUserLog()
-//
-//   res.render('customerList', { customers })
-// })
 
-
-//case 2
 router.get('/customerList', async (req, res) => {
   let customers = await Customer.findAll({raw: true})
+  const products = await Product.findAll({raw: true})
 
-  for (const customer of customers) {
-    let favoriteProduct = await Product.findByPk(customer.favoriteId, {raw: true})
+  customers = customers.map(customer => {
+    const favoriteProduct = products.find(product => customer.favoriteId === product.id)
     customer.productName = favoriteProduct.name
-  }
+    return customer
+  })
 
   //模擬其他資料邏輯處理
   await saveUserLog()
 
-  res.render('customerList', {customers})
+  res.render('customerList', { customers })
 })
+
+
+//case 2
+// router.get('/customerList', async (req, res) => {
+//   const customers = await Customer.findAll({raw: true})
+//
+//   for (const customer of customers) {
+//     const favoriteProduct = await Product.findByPk(customer.favoriteId, {raw: true})
+//     customer.productName = favoriteProduct.name
+//   }
+//
+//   //模擬其他資料邏輯處理
+//   await saveUserLog()
+//
+//   res.render('customerList', {customers})
+// })
 
 
 function saveUserLog() {
